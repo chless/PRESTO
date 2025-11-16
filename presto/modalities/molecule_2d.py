a8c6172901a438fe8b2e3c514d09152787f98d62
chless <chelssling@gmail.com>
2025-11-16 15:38:11 +0900

    fix the embedding dimension related to torch_geometric version issue

--- presto/modalities/molecule_2d.py
+++ presto/modalities/molecule_2d.py
@@ -27,6 +27,30 @@ NUM_CHIRALITY_TAG = 3
 NUM_BOND_TYPE = 6       # including aromatic and self-loop edge, and extra masked tokens
 NUM_BOND_DIRECTION = 3
 
+from ogb.utils.features import get_atom_feature_dims, get_bond_feature_dims
+
+
+class AtomEncoder(torch.nn.Module):
+
+    def __init__(self, emb_dim):
+        super(AtomEncoder, self).__init__()
+        full_atom_feature_dims = get_atom_feature_dims()
+        # pretrained moleculestm ckpt use different atom feature dims
+        full_atom_feature_dims[1] = 4
+
+        self.atom_embedding_list = torch.nn.ModuleList()
+
+        for i, dim in enumerate(full_atom_feature_dims):
+            emb = torch.nn.Embedding(dim, emb_dim)
+            torch.nn.init.xavier_uniform_(emb.weight.data)
+            self.atom_embedding_list.append(emb)
+
+    def forward(self, x):
+        x_embedding = 0
+        for i in range(x.shape[1]):
+            x_embedding += self.atom_embedding_list[i](x[:, i])
+
+        return x_embedding
 
 class Molecule2DModality(Modality):
     def __init__(
